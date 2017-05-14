a9ab464dd7c95d9fb6ca4e545f3c92d4c93f2e79
Alex Winger <awinger19@yahoo.com>
2017-05-13 22:51:56 -0700

    Descriptions of markers is spoken

--- CPPCampusMapBx/app/src/main/java/cs499android/com/cppmapbox/MarkerSelected.java
+++ CPPCampusMapBx/app/src/main/java/cs499android/com/cppmapbox/MarkerSelected.java
@@ -2,6 +2,7 @@ package cs499android.com.cppmapbox;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,20 +11,29 @@ import android.widget.ImageView;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Locale;
 
-public class MarkerSelected extends AppCompatActivity {
+public class MarkerSelected extends AppCompatActivity
+{
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marker_selected);
-        String title = getIntent().getExtras().getString("Title");
-        String description = getIntent().getExtras().getString("Description");
+        final String title = getIntent().getExtras().getString("Title");
+        final String description = getIntent().getExtras().getString("Description");
         EditText editText = (EditText) findViewById(R.id.title);
         editText.setText(title);
         editText = (EditText) findViewById(R.id.description);
         editText.setText(getDescription(description));
         setPicture(description);
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                textToSpeech.speak(getDescription(description), TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
     }
 
     public void setPicture(String description)
@@ -47,6 +57,8 @@ public class MarkerSelected extends AppCompatActivity {
 
     public void cancel(View view)
     {
+        if(textToSpeech.isSpeaking())
+            textToSpeech.stop();
         finish();
     }
 
